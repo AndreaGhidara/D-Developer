62d434c87cb3c91dd62f9f2fd5abd05b3ef6f2cb
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-08-31 10:53:40 +0200

    fix name surname devlist

--- src/pages/AppDevList.vue
+++ src/pages/AppDevList.vue
@@ -61,7 +61,7 @@ export default {
                             <!-- dati -->
                             <div class="col-md-4">
                                 <div class="card-body">
-                                    <h2 class="card-title">{{ item.name }},{{ item.surname }}</h2>
+                                    <h2 class="card-title">{{ item.name }} {{ item.surname }}</h2>
                                     <p class="card-text"><small class="text-body-secondary">{{item.date_of_birth}}</small></p>
                                     <p class="card-text"><small class="text-body-secondary"><span class="fw-semibold">Email:</span> {{item.email}}</small></p>
                                     <p class="card-text"><small class="text-body-secondary"><span class="fw-semibold">Numero di telefono:</span> {{item.phone_number}}</small></p>
